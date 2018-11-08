4e3152c03fffe9029e6b81e00ea3e8e4d7e3569f
Andrea Benfatti <welldone2094@gmail.com>
2018-11-08 17:04:01 +0100

    Planet drang and drop

---
+++ Assets/Scripts/DragDrop.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDrop : MonoBehaviour, IDragHandler, IEndDragHandler {
+
+    public void OnDrag(PointerEventData eventData)
+    {
+		Vector3 pz = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		pz.z = 0;
+        transform.position = pz;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+    }
+
+}
