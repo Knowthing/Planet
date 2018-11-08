8339012462336fbc0ca934091843415b2e940381
Andrea Benfatti <welldone2094@gmail.com>
2018-11-08 18:22:56 +0100

    Add gravity

---
+++ Assets/Scripts/Gravity.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour {
+
+    public float G = 100f;
+    public GameObject[] targets = null;
+
+    private void FixedUpdate()
+    {
+        if (targets != null) {
+            foreach ( GameObject target in targets) {
+                Attract(target);
+            }
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Attract(GameObject target) {
+        Rigidbody2D targetRb = target.GetComponent<Rigidbody2D>();
+        Rigidbody2D thisRb = this.GetComponent<Rigidbody2D>();
+        Vector3 direction = thisRb.position - targetRb.position;
+        float distance = direction.magnitude;
+
+        float forceMagnitude = (thisRb.mass * targetRb.mass) / Mathf.Pow(distance, 2);
+        Vector3 force = direction.normalized * forceMagnitude;
+
+        targetRb.AddForce(force);
+    }
+}
---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    public Vector2 speed;
+    Rigidbody2D rb;
+
+
+    // Use this for initialization
+    void Start () {
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.velocity = speed;
+    }
+	
+}
